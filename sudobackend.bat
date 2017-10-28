61989bcd027f9f8a93964ec2d47abd4ab272cc0e
Ivanq <imachug@yandex.ru>
2017-10-28 11:36:26 +0300

    Use absolute path for fifoid

--- sudobackend.bat
+++ sudobackend.bat
@@ -1,7 +1,12 @@
 @echo off
 cd %~dp0
 
-set fifoid=%1
+set fifoid=%~dp0%1
+
+set bash_fifoid=/%fifoid%
+set bash_fifoid=%bash_fifoid:\=/%
+set bash_fifoid=%bash_fifoid::=%
+
 <"%fifoid%.command" set /p command_to_run=
 del "%fifoid%.command"
 
@@ -12,4 +17,4 @@ del "%fifoid%.bash"
 del "%fifoid%.tty"
 
 "%bash_path%" -c "(%command_to_run%) >%frontend_tty% 2>&1 <%frontend_tty%"
-"%bash_path%" -c "echo 1 >%fifoid%.finish"
+"%bash_path%" -c "echo 1 >%bash_fifoid%.finish"
