14429124c95bf0f981e5de90558ff780b5e19f6e
Ivanq <imachug@yandex.ru>
2017-10-29 17:00:02 +0300

    Handle SIGINT correctly

--- sudobackend.sh
+++ sudobackend.sh
@@ -12,5 +12,16 @@ rm "$fifoid.fd"
 pid=$!
 echo $pid >"$fifoid.pidf"
 
+while true; do
+	sigint=$(cat "$fifoid.sigint")
+	if [ ! -z "$sigint" ]; then
+		if [ "$sigint" -eq 2 ]; then
+			break
+		fi
+		kill -s SIGINT $pid
+	fi
+done &
+
 wait $pid
+echo 2 >"$fifoid.sigint"
 echo 1 >"$fifoid.finish"
