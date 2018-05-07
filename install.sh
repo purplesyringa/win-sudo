acb5604ca37e472919c2237bce1c6600e28dc384
DemonixCraft <41BrRisack@gmail.com>
2018-05-07 19:24:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ function doInstall {
 	else
 		echo dir '~/bin' exist
 	fi
-	git clone https://github.com/imachug/win-sudo.git/s/ ~/bin
-	git clone https://github.com/imachug/win-sudo.git/install.sh /tmp
+	git clone https://github.com/Bradley/win-sudo1.git/s/ ~/bin
+	git clone https://github.com/Bradley/win-sudo1.git/install.sh /tmp
 }
 
 function doFail {
