8f4d42bbaf5f342d91a70577e0745d70181782a0
DemonixCraft <41BrRisack@gmail.com>
2018-05-08 14:59:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ function doInstall {
 	if ! [ -L ~/bin/win-sudo ] && ! [ -d ~/bin/win-sudo ]; then
 		mkdir ~/bin/win-sudo
 	fi
-	git clone https://github.com/DemonixCraft/win-sudo.git/s ~/bin/win-sudo
+	git clone ssh://github.com/DemonixCraft/win-sudo.git/s ~/bin/win-sudo
 }
 function doFail {
 	echo "failed to install, sorry :'(";
