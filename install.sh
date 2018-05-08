e102d525815d6475e06a2f428e6372272574d5ce
DemonixCraft <41BrRisack@gmail.com>
2018-05-08 18:50:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ function doInstall {
 		mkdir ~/bin/win-sudo
 	fi
 	git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo/
-	git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | cp('*.txt', '~/bin/win-sudo') && rm -rq *
+	git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | mv * ~/bin/win-sudo
 }
 function doFail {
 	echo "failed to install, sorry :'(";
