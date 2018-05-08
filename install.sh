7abb28dba797bed7423c1de65bd49e03b2f6907a
DemonixCraft <41BrRisack@gmail.com>
2018-05-08 16:30:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ function doInstall {
 		mkdir ~/bin/win-sudo
 	fi
 	#git clone https://github.com/DemonixCraft/win-sudo.git/s ~/bin/win-sudo
-	git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo | git rm img/screenshot.gif README.md install.sh
+	git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo | git rm -q img/screenshot.gif README.md install.sh
 	#git rm img/screenshot.gif README.md install.sh
 }
 function doFail {
