0ae103db68d83562dc39d5de5a38ae7af456da22
DemonixCraft <41BrRisack@gmail.com>
2018-05-09 10:28:18 -0400

    Git Sparse Checkout Testing
    I managed to maybe get sparse checkout to work but it needs a lot of testing. 
    
    Also I found out that PATH can be ~/bin/win-sudo instead of /c/Users/win-sudo. The problem is that the script doesn't consider '/c' to be a valid directory so it needs a rework. 
    
    The commented 'git clone' lines work fine with this script and I managed to do a successful install using my fork!

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ function doInstall {
 	fi
 	#git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo/
 	#git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | git mv ~/bin/win-sudo/s/*.* ..
-	cd ~/bin/win-sudo && git init -q | git config core.sparsecheckout true
-	echo s/ >> .git/info/sparse-checkout | git remote add -f origin https://github.com/DemonixOmega/win-sudo.git
-	git pull origin master
+	cd ~/bin/win-sudo && git init -q && git config core.sparsecheckout true
+	echo s/ >> .git/info/sparse-checkout && git remote add -f origin https://github.com/DemonixOmega/win-sudo.git | git pull origin master
 	
 	
 	
@@ -19,7 +18,7 @@ function doFail {
 	exit 1
 }
 doInstall || doFail
-PATH=$PATH:~/bin/win-sudo/s #/*/Users/*/bin/win-sudo/s
+PATH=$PATH:~/bin/win-sudo/s
 echo "enjoy :)"
 
 #Commented 'git clone' lines work 
