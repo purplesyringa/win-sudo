61a634ca994599acb53f2693e25de1b0a039a68b
DemonixCraft <41BrRisack@gmail.com>
2018-05-09 16:18:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ function doInstall {
 	if ! [ -L ~/bin/win-sudo ] && ! [ -d ~/bin/win-sudo ]; then
 		mkdir ~/bin/win-sudo
 	fi
-	cd ~/bin/win-sudo | git init -q && git config core.sparsecheckout true && git config core.bare false 
+	cd ~/bin/win-sudo
+	git init -q
+	git config core.sparsecheckout true
+	git config core.bare false 
 	echo s/ >> .git/info/sparse-checkout
 	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git
 	git pull origin master 
