ce39529dd83579491a85d82a4b749bc117056bdc
DemonixCraft <41BrRisack@gmail.com>
2018-05-09 10:22:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,15 @@ function doInstall {
 	if ! [ -L ~/bin/win-sudo ] && ! [ -d ~/bin/win-sudo ]; then
 		mkdir ~/bin/win-sudo
 	fi
-	git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo/
-	git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | git mv ~/bin/win-sudo/s/*.* ..
+	#git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo/
+	#git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | git mv ~/bin/win-sudo/s/*.* ..
+	cd ~/bin/win-sudo && git init -q | git config core.sparsecheckout true
+	echo s/ >> .git/info/sparse-checkout | git remote add -f origin https://github.com/DemonixOmega/win-sudo.git
+	git pull origin master
+	
+	
+	
+	
 }
 function doFail {
 	echo "failed to install, sorry :'(";
@@ -14,3 +21,5 @@ function doFail {
 doInstall || doFail
 PATH=$PATH:~/bin/win-sudo/s #/*/Users/*/bin/win-sudo/s
 echo "enjoy :)"
+
+#Commented 'git clone' lines work 
