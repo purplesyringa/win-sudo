1f3322acb1b5ede77a7b5731ec82f5643bd5f266
DemonixCraft <41BrRisack@gmail.com>
2018-05-09 15:28:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,9 @@ function doInstall {
 	fi
 	cd ~/bin/win-sudo | git init -q && git config core.sparsecheckout true
 	echo s/ >> .git/info/sparse-checkout
-	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git && git pull origin master 
+	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git 
+	git pull origin master 
 	git remote rm origin
-	
-	
-	
-	
 }
 function doFail {
 	echo "failed to install, sorry :'(";
