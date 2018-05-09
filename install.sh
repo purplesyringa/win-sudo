d9c493ee9f3914a020fb2716302afb61eba99c03
DemonixCraft <41BrRisack@gmail.com>
2018-05-09 15:05:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-shopt -s globstar
+#shopt -s globstar
 function doInstall {
 	if ! [ -L ~/bin/win-sudo ] && ! [ -d ~/bin/win-sudo ]; then
 		mkdir ~/bin/win-sudo
 	fi
-	#git clone https://github.com/DemonixCraft/win-sudo.git ~/bin/win-sudo && cd ~/bin/win-sudo/
-	#git rm -rq *.* && cd ~/bin/win-sudo/img | git rm -rq *.* && cd ~/bin/win-sudo/s | git mv ~/bin/win-sudo/s/*.* ..
-	cd ~/bin/win-sudo 
-	git init -q 
-	git config core.sparsecheckout true
-	echo s/ >> .git/info/sparse-checkout 
-	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git 
-	git pull origin master
+	cd ~/bin/win-sudo | git init -q && git config core.sparsecheckout true
+	echo s/ >> .git/info/sparse-checkout | git remote add -f origin https://github.com/DemonixCraft/win-sudo.git && git pull origin master
 	
 	
 	
