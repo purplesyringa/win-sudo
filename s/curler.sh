a14c086940b236d8e74def9b8bd0f7770b0d40a1
DemonixCraft <41BrRisack@gmail.com>
2018-05-10 17:59:42 -0400

    Simplified curler.sh
    Just made it a lot simpler

--- s/curler.sh
+++ s/curler.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-function curl {
-	curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/install.sh | sh
-}
-function curlfail {
-  echo "Curl failed...check your connection to the network"
-  exit 1
-}
-curl | | curlfail
+curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/install.sh | sh
