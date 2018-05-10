dbc42089f3c2f91e72712812a2cc4c39ad29bc7f
DemonixCraft <41BrRisack@gmail.com>
2018-05-10 17:42:46 -0400

    Update curler.sh

--- curler.sh
+++ curler.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-function curl {
-	curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/install.sh | sh
-}
-function curlfail {
-  echo "Curl failed...check your connection to the network"
-  exit 1
-}
-curl | | curlfail
+
