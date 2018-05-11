bda491c8940687745067951d969fa710a0c2e7d6
DemonixCraft <41BrRisack@gmail.com>
2018-05-10 20:25:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function doInstall {
 	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git
 	git pull origin master 
 	git remote rm origin
-	extra_directories=(/*/Users/*/bin/win-sudo/s)
+	extra_directories=(~/bin/win-sudo/s)
 	extra_path=$(printf "%s:" "${extra_directories[@]}")
 	export PATH="${extra_path}${PATH}" >> ~/.bashrc
 	source ~/.bashrc
