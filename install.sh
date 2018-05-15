ecb76162f93eeabef6b7042ff0779a591f367edd
DemonixCraft <41BrRisack@gmail.com>
2018-05-15 15:30:42 -0400

    Removed four comments

--- install.sh
+++ install.sh
@@ -7,10 +7,6 @@ function doInstall {
 	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git
 	git pull origin master 
 	git remote rm origin
-	#extra_directories=(/c/Users/*/bin/win-sudo/s)
-	#extra_path=$(printf "%s:" "${extra_directories[@]}")
-	#export PATH="${extra_path}${PATH}" >> ~/.bashrc
-	#____________________________________________________
 	#cat ~/bin/win-sudo/s/lines.txt >> ~/.bashrc
 	#echo -e "export PATH=~/bin/win-sudo/s:$PATH" >> ~/.bashrc
 	export PATH="$PATH:~/bin/win-sudo/s" >> ~/.bash_profile
