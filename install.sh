4430dcb48f1524d4911bd15ec59d0f792f3f02d8
DemonixCraft <41BrRisack@gmail.com>
2018-05-15 16:34:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ function doInstall {
 	git remote rm origin
 	#cat ~/bin/win-sudo/s/lines.txt >> ~/.bashrc
 	#echo -e "export PATH=~/bin/win-sudo/s:$PATH" >> ~/.bashrc
-	echo "source ~/bin/win-sudo/s/lines" | tee -a ~/.bashrc
+	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc
 	source ~/.bashrc
 	#NEEDS A LOT MORE TESTING. WILL BE MUCH MORE COMPRESSED WHEN FINISHED
 }
