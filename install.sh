3e4fd6fd0d41f9d85606aa8147bb37118dd3a5bd
DemonixCraft <41BrRisack@gmail.com>
2018-05-16 14:47:50 -0400

    TEST : Removed 'source'

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ function doInstall {
 	git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 	git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
 	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from lines.sh upon startup
-	source ~/.bashrc #TESTING COMMAND : Remove if unneeded
 	. ~/.bashrc #Refreshes ~/.bashrc so effect will occur
 	#WILL BE MUCH MORE COMPRESSED WHEN FINISHED
 	#Warning : This will not work if '.bash_profile' in in your $HOME directory, as it will not source ~/bashrc upon startup
