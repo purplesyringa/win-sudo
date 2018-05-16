c2e1651da6a3dfda424437fe850b83ea2a070f7c
DemonixCraft <41BrRisack@gmail.com>
2018-05-15 21:07:14 -0400

    Removed some comments

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ function doInstall {
 	git remote add -f origin https://github.com/DemonixCraft/win-sudo.git
 	git pull origin master 
 	git remote rm origin
-	#cat ~/bin/win-sudo/s/lines.txt >> ~/.bashrc
-	#echo -e "export PATH=~/bin/win-sudo/s:$PATH" >> ~/.bashrc
 	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc
 	source ~/.bashrc
 	. ~/.bashrc
