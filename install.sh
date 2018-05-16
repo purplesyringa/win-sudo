afc08193579666dece5fca69b46c3a3d142cd2d5
DemonixCraft <41BrRisack@gmail.com>
2018-05-16 18:04:43 -0400

    TEST : ORIGIN MASTER #2

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ function doInstall {
 	mkdir -pv ~/bin/win-sudo && cd "$_"; git init -q
 	git config core.sparsecheckout true
 	echo s/ >> .git/info/sparse-checkout
-	git remote add -f origin/master https://github.com/DemonixCraft/win-sudo.git
+	git remote add -f origin master https://github.com/DemonixCraft/win-sudo.git
 	git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 	git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
 	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from lines.sh upon startup
