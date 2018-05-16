d772c318773e721d68019a4a6ec230f05a298e19
DemonixCraft <41BrRisack@gmail.com>
2018-05-15 21:00:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ function doInstall {
 	#echo -e "export PATH=~/bin/win-sudo/s:$PATH" >> ~/.bashrc
 	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc
 	source ~/.bashrc
+	. ~/.bashrc
 	#WILL BE MUCH MORE COMPRESSED WHEN FINISHED
 }
 function doFail {
