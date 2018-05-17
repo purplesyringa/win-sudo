1f49e45069ead9f802bd1255b94a06f5d4622b18
DemonixCraft <41BrRisack@gmail.com>
2018-05-17 15:12:11 -0400

    Added silencer and fail silencer options

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/doFail.sh | bash
+curl -sf https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/doFail.sh | bash
 trap 'doFail' ERR #If any of the following lines output 0, run doFail and exit
 mkdir -pv ~/bin/win-sudo && cd "$_"; git init -q #Create dir > Navigate to new dir > Quietly initiate a git repo in current dir
 git config core.sparsecheckout true #Sets 'sparsecheckout' boolean to 'true'
