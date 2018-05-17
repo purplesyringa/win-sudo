3f89564119f20d3b75cdfd21ca3d108b89c222ef
DemonixCraft <41BrRisack@gmail.com>
2018-05-16 21:01:20 -0400

    Update removal path

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ function doInstall {
 	git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 	git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
 	echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from path.sh upon startup
-	cd ~/bin/win-sudo/s
-	git rm -rf .git
+	git rm -rf ~/bin/win-sudo/s/.git/*
 	#Warning : This will not work if '.bash_profile' in in your $HOME directory, as it will not source ~/bashrc upon startup
 }
 function doFail {
