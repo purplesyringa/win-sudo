5ab7e274b3f8665d5da87ed3eda0fd57276c236d
DemonixCraft <41BrRisack@gmail.com>
2018-05-17 13:22:31 -0400

    Added some Dev Comments (TEMP)

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-source <(curl -s https://github.com/DemonixCraft/win-sudo/master/doFail.sh)
-trap 'doFail' ERR
-mkdir -pv ~/bin/win-sudo && cd "$_"; git init -q
-git config core.sparsecheckout true
-echo s/ >> .git/info/sparse-checkout
-git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git
+source <(curl -s https://github.com/DemonixCraft/win-sudo/master/doFail.sh) #Gets doFail function from doFail.sh
+trap 'doFail' ERR #If any of the following lines output 0, run doFail and exit
+mkdir -pv ~/bin/win-sudo && cd "$_"; git init -q #Create dir > Navigate to new dir > Quietly initiate a git repo in current dir
+git config core.sparsecheckout true #Sets 'sparsecheckout' boolean to 'true'
+echo s/ >> .git/info/sparse-checkout #Send line "s/" to info/sparse-checkout
+git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git #Add url to origin
 git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from path.sh upon startup
