de188c5b43e887db95bc53d3c767977680916edc
DemonixCraft <41BrRisack@gmail.com>
2018-05-16 20:05:05 -0400

    Tampered install.sh to support the new filename

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ function doInstall {
 	git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git
 	git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 	git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
-	echo "source ~/bin/win-sudo/s/lines.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from lines.sh upon startup
+	echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from path.sh upon startup
 	#Warning : This will not work if '.bash_profile' in in your $HOME directory, as it will not source ~/bashrc upon startup
 }
 function doFail {
