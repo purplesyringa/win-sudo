e707d75b3ca510d342106993bdc31b95dcf891a8
DemonixCraft <41BrRisack@gmail.com>
2018-05-17 17:21:53 -0400

    TEST : Suppress echo output

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo s/ >> .git/info/sparse-checkout #Send line "s/" to info/sparse-checkout
 git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git #Add url to origin
 git pull -q origin master #Fetches new commits from Master Branch of project and doesn't show output
 git remote rm origin #Temp Command : Removes Origin so it will not mess up next install
-echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc #Tells ~/.bashrc to use the code from path.sh upon startup
+echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1 #Tells ~/.bashrc to use the code from path.sh upon startup
 #Warning : This will not work if '.bash_profile' in in your $HOME directory, as it will not source ~/bashrc upon startup
 echo "enjoy :)"
 exec bash #Refreshes current shell
