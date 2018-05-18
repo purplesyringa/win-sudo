343dda4ec004e5199e24f5e6ae56b3036fde7eda
DemonixCraft <41BrRisack@gmail.com>
2018-05-17 20:13:05 -0400

    Unverbose 'mkdir'

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Downloading win-sudo..."
 trap 'echo "Failed to install, sorry :(";exit 1' ERR 
-mkdir -pv ~/bin/win-sudo && cd "$_"; git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
+mkdir -p ~/bin/win-sudo && cd "$_"; git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git && git pull -q origin master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
 rm -rf ~/.bash_profile >/dev/null 2>&1
