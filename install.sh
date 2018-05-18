6ebd763867ebfa89d1536d38f509413f692d3169
DemonixCraft <41BrRisack@gmail.com>
2018-05-18 10:02:57 -0400

    L6 : Switched order of cmds + mvd 'exec bash' back

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' 
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git && git pull -q origin master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
-rm -rf ~/.bash_profile >/dev/null 2>&1;echo "Win-sudo successfully installed!"
-exec bash
+echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bash
