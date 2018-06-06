33bd0032939759c3d681f938c2340dd2d18e80d6
DemonixCraft-1 <39924761+DemonixCraft-1@users.noreply.github.com>
2018-06-06 16:44:15 -0400

    FIX : I was an idiot
    I copied and pasted my OUTPUT for the code that I put in. Really sorry I don't want to close this again so i'll stick with two commits.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo-test$ mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
+#!/usr/bin/env bash
+echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' ERR 
+mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
+git remote add -mf origin https://github.com/DemonixCraft/win-sudo.git && git pull -q origin master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
-echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bashBradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ git remote add -mf origin https:.com/DemonixCraft/win-sudo.git && git pull -q origin master
-fatal: remote origin already exists.
-Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
-Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bash
+echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bash
