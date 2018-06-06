aee7c516dd62927ecaa58747783ab898e050a594
DemonixOmega <41BrRisack@gmail.com>
2018-06-06 16:20:17 -0400

    compressed a lot of code + automatic path

--- install.sh
+++ install.sh
@@ -1,20 +1,6 @@
-#!/usr/bin/env bash
-function doInstall {
-	if ! [ -L ~/bin ] && ! [ -d ~/bin ]; then
-		echo make dir
-		mkdir ~/bin
-	else
-		echo dir '~/bin' exist
-	fi
-	cd /tmp && \
-	git clone https://github.com/imachug/win-sudo.git && \
-	mv win-sudo/su* ~/bin && \
-	rm -rf win-sudo
-}
-
-function doFail {
-	echo "failed to install, sorry :'(";
-	exit 1
-}
-doInstall || doFail
-echo "enjoy :)"
+Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo-test$ mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
+echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
+echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bashBradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ git remote add -mf origin https:.com/DemonixCraft/win-sudo.git && git pull -q origin master
+fatal: remote origin already exists.
+Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc >/dev/null 2>&1
+Bradley@WINDOWS-CTR6ND1:~/bin/win-sudo$ echo "Win-sudo successfully installed!";rm -rf ~/.bash_profile >/dev/null 2>&1;exec bash
