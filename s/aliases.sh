4f3ab68bec7b8475ac74b654737296e9bebac20d
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 13:20:33 -0400

    Add ws-success alias

--- s/aliases.sh
+++ s/aliases.sh
@@ -2,5 +2,6 @@
 shopt -s expand_aliases
 #Win-sudo Installation Aliases : 
 alias gramf="git remote add -mf base"
+alias ws-success='echo "Win-sudo successfully installed!";exec bash'
 #Win-sudo Usage Aliases : 
 alias wi="winpty" #Usage : sudo wi [command]
