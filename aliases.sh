2c0ee29934914429f1a62ea428b29fe8ec3812de
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 16:05:10 -0400

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -6,5 +6,3 @@ alias ws-success='echo "Win-sudo successfully installed!";exec bash'
 alias ws-script-start='echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' ERR'
 alias ws-files="~/.bashrc ~/.bash_profile /etc/profile"
 alias null=">/dev/null 2>&1"
-#Win-sudo Usage Aliases : 
-alias wi="winpty" #Usage : sudo wi [command]
