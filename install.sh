8eeb761c0ea890603d10524998a3fd9fe49dd982
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 13:23:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/imachug/win-sudo/s/aliases.sh)
-echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' ERR
+ws-start-script
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 gramf https://github.com/imachug/win-sudo.git && git pull -q base master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc ~/.bash_profile /etc/profile >/dev/null 2>&1
