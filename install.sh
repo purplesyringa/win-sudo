8fd66ea211a42f0ab4ae522b2e1c3ee62bab479c
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 12:47:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' 
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 git remote add -mf origin https://github.com/imachug/win-sudo.git && git pull -q origin master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc ~/.bash_profile /etc/profile >/dev/null 2>&1
-echo "source ~/bin/win-sudo/s/wi.sh" >>~/.bashrc
+echo "source ~/bin/win-sudo/s/aliases.sh" >>~/.bashrc
 echo "Win-sudo successfully installed!";exec bash
