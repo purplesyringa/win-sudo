f2326b749a029941cb1ce5f271f74061864fa064
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 13:15:45 -0400

    Add gramf alias

--- s/aliases.sh
+++ s/aliases.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
+#Win-sudo Installation Aliases : 
+alias gramf="git remote add -mf"
+#Win-sudo Usage Aliases : 
 alias wi="winpty" #Usage : sudo wi [command]
