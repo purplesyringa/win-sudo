6fa1423fbc3e80ab863a1402c25e1b937423b7c6
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 18:41:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases.sh && ./installaliases.sh && ws-start-script
+source (curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases.sh) && ./installaliases.sh && ws-start-script
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 gramf https://github.com/imachug/win-sudo.git && git pull -q base master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null
