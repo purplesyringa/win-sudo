544d6c6a3f190074281c981aa38e275ca1cfa3bd
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 13:23:02 -0400

    Add ws-start-script alias

--- s/aliases.sh
+++ s/aliases.sh
@@ -3,5 +3,6 @@ shopt -s expand_aliases
 #Win-sudo Installation Aliases : 
 alias gramf="git remote add -mf base"
 alias ws-success='echo "Win-sudo successfully installed!";exec bash'
+alias ws-script-start='echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' ERR'
 #Win-sudo Usage Aliases : 
 alias wi="winpty" #Usage : sudo wi [command]
