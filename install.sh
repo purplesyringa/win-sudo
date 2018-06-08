39798451622043b4e2c4b0f0e01f0fe0a635370c
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 18:51:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl --insecure -o installaliases.sh https://raw.github.com/gurjeet/pg_dev_env/master/installaliases.sh && ws-start-script
+curl -os installaliases.sh https://raw.github.com/gurjeet/pg_dev_env/master/installaliases.sh && source ./installaliases.sh && ws-start-script
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 gramf https://github.com/imachug/win-sudo.git && git pull -q base master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null
