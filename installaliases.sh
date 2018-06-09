12bfa3a453bddbfede5fedae65488477fd51bbc6
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 20:03:09 -0400

    Update installaliases.sh

--- installaliases.sh
+++ installaliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-shopt -s expand_aliases
 function ws-script-start() {
-  echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :("; exit 1' ERR
+  echo "Downloading win-sudo..."
+  trap 'echo "Failed to install, sorry :("; exit 1' ERR
 }
 alias gramf="git remote add -mf base"
 alias ws-success='echo Win-sudo successfully installed!;exec bash'
