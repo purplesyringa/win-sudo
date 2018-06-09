d47c91bc6c20360d5e105686f8f7da9ef9aa6766
DemonixCraft <41BrRisack@gmail.com>
2018-06-08 20:02:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+shopt -s expand_aliases
 ws-start-script
 mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
 gramf https://github.com/imachug/win-sudo.git && git pull -q base master
