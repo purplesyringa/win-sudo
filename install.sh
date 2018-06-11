5f19e023138de1b5ddd8b1bfbce44c5ee2249e38
DemonixCraft <41BrRisack@gmail.com>
2018-06-11 17:27:56 -0400

    Added installation/regular aliases/functions

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "Downloading win-sudo...";trap 'echo "Failed to install, sorry :(";exit 1' ERR
-mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
-git remote add -mf origin https://github.com/imachug/win-sudo.git && git pull -q origin master
-echo "source ~/bin/win-sudo/s/path.sh" | tee -a ~/.bashrc ~/.bash_profile /etc/profile >/dev/null 2>&1
-echo "Win-sudo successfully installed!";exec bash
+shopt -s expand_aliases
+cat <(curl -s https://raw.githubusercontent.com/imachug/win-sudo/master/installaliases) >> ~/.bashrc && . ~/.bashrc;ws-start-script
+mkdir -pv ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
+echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null && echo "source ~/bin/win-sudo/s/aliases" >>~/.bashrc && ws-success
