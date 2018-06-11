d865491600fad48d52b70af4418c755c87697df7
DemonixCraft <41BrRisack@gmail.com>
2018-06-11 15:07:29 -0400

    Update installaliases.sh

--- installaliases.sh
+++ installaliases.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-cat <(curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases.sh) | bash
+cat <(curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases.sh) | tee -a ~/.bashrc && . ~/.bashrc
 function ws-script-start {
   echo "Downloading win-sudo..."
   trap 'echo "Failed to install, sorry :("; exit 1' ERR
 }
+function ws-success {
+  echo Win-sudo successfully installed!
+  exec bash
+}
 alias gramf="git remote add -mf base"
-alias ws-success='echo Win-sudo successfully installed!;exec bash'
 alias ws-files="~/.bashrc ~/.bash_profile /etc/profile"
 alias null=">/dev/null 2>&1"
