c2cfc7de18b0447468412a58eb9ad4d7a849b5bd
DemonixCraft-1 <39924761+DemonixCraft-1@users.noreply.github.com>
2018-06-11 17:23:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 shopt -s expand_aliases
 cat <(curl -s https://raw.githubusercontent.com/imachug/win-sudo/master/installaliases) >> ~/.bashrc && . ~/.bashrc;ws-start-script
 mkdir -pv ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
-echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null && echo "source ~/bin/win-sudo/s/aliases.sh" >>~/.bashrc && ws-success
+echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null && echo "source ~/bin/win-sudo/s/aliases" >>~/.bashrc && ws-success
