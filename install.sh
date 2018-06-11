4886cc0177017c993f2b58e5b2652eff528e0bbe
DemonixCraft <41BrRisack@gmail.com>
2018-06-11 15:37:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 shopt -s expand_aliases
 cat <(curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases) >> ~/.bashrc && . ~/.bashrc
 ws-start-script
-mkdir -p ~/bin/win-sudo && cd "$_";git init -q && git config core.sparsecheckout true;echo s/ >> .git/info/sparse-checkout
-gramf https://github.com/imachug/win-sudo.git && git pull -q base master
+mkdir -p ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
 echo "source ~/bin/win-sudo/s/path.sh" | tee -a ws-files null && echo "source ~/bin/win-sudo/s/aliases.sh" >>~/.bashrc
 ws-success
