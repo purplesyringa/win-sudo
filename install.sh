da50d049093bf5bb97cd7915fd31bf9affd06ec6
DemonixCraft <41BrRisack@gmail.com>
2018-06-11 15:52:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o posix
-shopt -s expand_aliases extglob
+shopt -s expand_aliases extglob compat31 
 cat <(curl -s https://raw.githubusercontent.com/DemonixCraft/win-sudo/master/installaliases) >> ~/.bashrc && . ~/.bashrc
 ws-start-script
 mkdir -p ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
