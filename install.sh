7b374770a6f5b34b6cdae66f136ac5f83608c3b2
Ricardo Rodriguez <rdz.g.ricardo@gmail.com>
2018-08-20 00:42:05 -0500

    updated with the latest install aliases
    updated with the latest install aliases
    test first if install dir exist before removing it

--- install.sh
+++ install.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
+#Begin install aliases
 shopt -s expand_aliases
-function ws-script-start {
-  echo "Downloading win-sudo..."
-  trap 'echo "Failed to install, sorry :("; exit 1' ERR
-}
-function ws-success {
-  echo Win-sudo successfully installed!
-  exec bash
-  alias fresh="exec bash"
-}
-function init-gitrepo-setup {
-  git init -q
-  git config core.sparsecheckout true
-  echo s/ >> .git/info/sparse-checkout
-}
+alias ws-script-start="echo Downloading win-sudo...;trap 'echo Failed to install, sorry :(; exit 1' ERR"
+alias init-gitrepo-setup="git init -q && git config core.sparsecheckout true ; echo s/ >> .git/info/sparse-checkout"
+alias ws-success="echo Win-sudo successfully installed!;exec bash"
 alias gramf="git remote add -mf base"
 alias ws-files="~/.bashrc ~/.bash_profile /etc/profile"
 alias null=">/dev/null 2>&1"
+##End install alias
+
 ws-script-start
- rm -rfv ~/bin/win-sudo && mkdir -pv ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
+#clean default install dir
+if test -d ~/bin/win-sudo ; then
+  rm -rfv ~/bin/win-sudo
+fi  
+#download latest sources to a fresh directory 
+mkdir -pv ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
+#start processing bash initialization files
 for f in ~/.bashrc ~/.bash_profile #/.profile leaving it out to avoid permission issues atleast in git bash
 do
  echo "Processing $f"
