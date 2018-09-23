7125f5a44adaddcd93f2314b8b2c9fcef20c6d6a
Ivanq <imachug@yandex.ru>
2018-09-23 18:31:37 +0300

    Fix install.sh when no files exist

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Begin install aliases
+# Begin install aliases
 shopt -s expand_aliases
 alias ws-script-start="echo Downloading win-sudo...;trap 'echo Failed to install, sorry :(; exit 1' ERR"
 alias init-gitrepo-setup="git init -q && git config core.sparsecheckout true ; echo s/ >> .git/info/sparse-checkout"
@@ -7,17 +7,18 @@ alias ws-success="echo Win-sudo successfully installed!;exec bash"
 alias gramf="git remote add -mf base"
 alias ws-files="~/.bashrc ~/.bash_profile /etc/profile"
 alias null=">/dev/null 2>&1"
-##End install alias
+# End install alias
 
 ws-script-start
-#clean default install dir
+# clean default install dir
 if test -d ~/bin/win-sudo ; then
   rm -rfv ~/bin/win-sudo
 fi  
-#download latest sources to a fresh directory 
+# download latest sources to a fresh directory 
 mkdir -pv ~/bin/win-sudo && cd "$_";init-gitrepo-setup && gramf https://github.com/imachug/win-sudo.git && git pull -q base master
-#start processing bash initialization files
-for f in ~/.bashrc ~/.bash_profile #/.profile leaving it out to avoid permission issues atleast in git bash
+# start processing bash initialization files
+touch ~/.bashrc
+for f in ~/.bashrc ~/.bash_profile # /.profile leaving it out to avoid permission issues atleast in git bash
 do
  echo "Processing $f"
  if test -f $f ; then
